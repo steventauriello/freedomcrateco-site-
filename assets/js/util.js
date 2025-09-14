add6b74e39865882489fb76a87c3d6c74e74f4b6
steventauriello <stauriello86@gmail.com>
2025-09-14 14:45:34 -0400

    Update util.js

--- assets/js/util.js
+++ assets/js/util.js
@@ -14,3 +14,19 @@
     window.dispatchEvent(new Event('cart:updated'));
   };
 })();
+// Auto-set active nav link based on current page
+(function(){
+  let here = (location.pathname.split('/').pop() || 'index.html').toLowerCase();
+  // Treat product pages as part of "Shop"
+  const alias = { 'product.html': 'index.html' };
+  if (alias[here]) here = alias[here];
+
+  document.querySelectorAll('.nav a').forEach(a => {
+    const target = (a.getAttribute('href') || '').split('/').pop().toLowerCase();
+    if (target === here) {
+      a.classList.add('active');
+    } else {
+      a.classList.remove('active');
+    }
+  });
+})();
